9c9dfd89215bbf2c55622e717ae333b469bedc83
unknown <lingjifa@gmail.com>
2021-05-10 17:42:04 +0800

    增加server 访问接口

--- cloud/src/main/java/cloud/HttpServer.java
+++ cloud/src/main/java/cloud/HttpServer.java
@@ -20,9 +20,8 @@ import com.alibaba.fastjson.JSON;
 import cloud.bean.GetDeviceModelDoc;
 import cloud.bean.InsertMongoDBDoc;
 import base.model.bean.*;
-import base.model.connect.bean.KafkaConfig;
-import base.model.connect.bean.MqttConfig;
 
+import static akka.http.javadsl.server.PathMatchers.segment;
 /**
  * @author ：LLH
  * @date ：Created in 2021/4/19 11:22
@@ -55,13 +54,33 @@ public class HttpServer extends AllDirectives {
 //                                            complete("order created")
 //                                    );
 //                                }))),
+                get(()->
+                        pathPrefix("create-model-cc3200", ()->
+                                path(segment(), (String no)-> {
+                                    final CompletionStage<Done> futureStage = createCC3200Model(no);
+                                    return onSuccess(futureStage, done -> complete("success create cc3200 model" + no));
+                                }))),
 
-                path("create-model", () ->
-                        get(()-> {
-                            createModel();
-                            return complete("create model cc3200-1111 link succeed");
+                get(()->
+                        pathPrefix("link-device-no", ()->
+                                path(segment(), (String no)-> {
+                                    final CompletionStage<Done> futureStage = linkDeviceByNo(no);
+                                    return onSuccess(futureStage, done -> complete("success link device... " + no));
+                                }))),
 
-                        })),
+                get(()->
+                        pathPrefix("publish-event", ()->
+                                path(segment(), (String event)-> {
+                                    final CompletionStage<Done> futureStage = publishEventToDevice(event);
+                                    return onSuccess(futureStage, done -> complete("success link device... "));
+                                }))),
+
+//                path("create-model", () ->
+//                        get(()-> {
+//                            createModel();
+//                            return complete("create model cc3200-1111 link succeed");
+//
+//                        })),
 
                 path("link-device", () ->
                                 get(()-> {
@@ -118,37 +137,73 @@ public class HttpServer extends AllDirectives {
 //                );
     }
 
-//    private CompletionStage<Done> getKafkaMsg() {
-////        ActorRef<BasicCommon> httpRequestRef = system.systemActorOf(HttpRequestActor.create(brainControlActorRef),
-////                "http-request-ref", Props.empty());
-////
-////
-////        brainControlActorRef.tell(new GetKafkaMsg(httpRequestRef));
-//
-//    }
+    private CompletionStage<Done> publishEventToDevice(String event) {
 
-//    public static class HttpRequestActor extends AbstractBehavior<BasicCommon> {
-//
-//        public HttpRequestActor(ActorContext<BasicCommon> context, ActorRef<BasicCommon> brainControlActorRef) {
-//            super(context);
-//        }
-//
-//        @Override
-//        public Receive<BasicCommon> createReceive() {
-//            return newReceiveBuilder()
-//                    .onMessage(KafkaMsg.class, this::onHandleKafkaMsg)
-//                    .build();
-//        }
-//
-//        private Behavior<BasicCommon> onHandleKafkaMsg(KafkaMsg msg) {
-//
-//            return this;
-//        }
-//
-//        public static Behavior<BasicCommon> create(ActorRef<BasicCommon> brainControlActorRef) {
-//            return Behaviors.setup(context -> new HttpRequestActor(context, brainControlActorRef));
-//        }
-//    }
+
+        return CompletableFuture.completedFuture(Done.getInstance());
+    }
+
+    private CompletionStage<Done> linkDeviceByNo(String no) {
+        DeviceModel deviceModel = new DeviceModel();
+
+        GetDeviceModelDoc doc = new GetDeviceModelDoc();
+        doc.setKey("name");
+        doc.setValue("cc3200-" + no);
+        doc.setConnName("model");
+        doc.setCollectionName("model");
+        doc.setDeviceModel(deviceModel);
+        logger.log(Level.INFO, "device cc3200-" + no + " is up....");
+        mongoDBActorRef.tell(doc);
+        return CompletableFuture.completedFuture(Done.getInstance());
+    }
+
+    private CompletionStage<Done> createCC3200Model(String no) {
+        System.out.println("create model " + no);
+
+        InsertMongoDBDoc doc = new InsertMongoDBDoc();
+        Map<String,String> map = new HashMap<>();
+        AbstractModel model = new AbstractModel();
+        model.setName("cc3200");
+        model.setNo(no);
+
+        List<Property> properties = new ArrayList<>();
+        Property property1 = new Property();
+        property1.setName("humidity");
+        properties.add(property1);
+
+        Property property2 = new Property();
+        property2.setName("temperature");
+        properties.add(property2);
+        model.setProperties(properties);
+
+        List<Event> events = new ArrayList<>();
+        Event event1 = new Event();
+        event1.setName("light-on");
+        events.add(event1);
+
+        Event event2 = new Event();
+        event2.setName("light-off");
+        events.add(event2);
+
+        model.setEvents(events);
+
+        DeviceModel deviceModel = new DeviceModel();
+        deviceModel.setModel(model);
+
+        String str = JSON.toJSONString(deviceModel);
+
+        map.put("name","cc3200-" + no);
+        map.put("model", str);
+        doc.setDocMap(map);
+        doc.setConnName("model");
+        doc.setCollectionName("model");
+//        System.out.println("11111");
+//        mongoDBActorRef.tell(doc);
+        return CompletableFuture.completedFuture(Done.getInstance());
+    }
+
+
+
 
 
     private CompletionStage<Done> linkDevice() {
@@ -166,81 +221,81 @@ public class HttpServer extends AllDirectives {
         return CompletableFuture.completedFuture(Done.getInstance());
     }
 
-    private CompletionStage<Done> createModel() {
-        InsertMongoDBDoc doc = new InsertMongoDBDoc();
-        Map<String,String> map = new HashMap<>();
-        AbstractModel model = new AbstractModel();
-        model.setName("cc3200");
-        model.setNo("1111");
-//        Profile profile = new Profile();
-//        profile.setProductName("");
-
-        List<Property> properties = new ArrayList<>();
-        Property property1 = new Property();
-        property1.setName("humidity");
-        property1.setMqttTopic("cc3200/1111/humidity");
-        properties.add(property1);
-
-        Property property2 = new Property();
-        property2.setName("temperature");
-        property2.setMqttTopic("cc3200/1111/temperature");
-        properties.add(property2);
-        model.setProperties(properties);
-
-        List<Event> events = new ArrayList<>();
-        Event event1 = new Event();
-        event1.setName("light-on");
-        event1.setMqttTopic("cc3200/1111/light-on");
-        events.add(event1);
-
-        Event event2 = new Event();
-        event2.setName("light-off");
-        event2.setMqttTopic("cc3200/1111/light-off");
-        events.add(event2);
-
-        model.setEvents(events);
-
-        DeviceModel deviceModel = new DeviceModel();
-        deviceModel.setModel(model);
-        KafkaConfig kafkaConfig = new KafkaConfig();
-        kafkaConfig.setServer("192.168.123.131:9092");
-        kafkaConfig.setGroupId("1");
-//        List<String> kafkaTopics = new ArrayList<>();
-        kafkaConfig.setTopic("cc3200/1111");
-//        kafkaConfig.setTopics(kafkaTopics);
-        deviceModel.setKafkaConfig(kafkaConfig);
-
-        MqttConfig mqttConfig = new MqttConfig();
-        mqttConfig.setBrokerUrl("tcp://192.168.123.247:1883");
-        mqttConfig.setClientId("12345");
-//        List<String> mqttTopics = new ArrayList<>();
-        mqttConfig.setTopic("cc3200/1111/#");
-//        mqttTopics.add("cc3200/1111/temperature");
-//        mqttConfig.setTopics( mqttTopics);
-        deviceModel.setMqttConfig(mqttConfig);
-
-
-        String str = JSON.toJSONString(deviceModel);
-
-        map.put("name","cc3200-1111");
-        map.put("model", str);
-        doc.setDocMap(map);
-        doc.setConnName("model");
-        doc.setCollectionName("model");
+//    private CompletionStage<Done> createModel() {
+//        InsertMongoDBDoc doc = new InsertMongoDBDoc();
+//        Map<String,String> map = new HashMap<>();
+//        AbstractModel model = new AbstractModel();
+//        model.setName("cc3200");
+//        model.setNo("1111");
+////        Profile profile = new Profile();
+////        profile.setProductName("");
+//
+//        List<Property> properties = new ArrayList<>();
+//        Property property1 = new Property();
+//        property1.setName("humidity");
+////        property1.setMqttTopic("cc3200/1111/humidity");
+//        properties.add(property1);
+//
+//        Property property2 = new Property();
+//        property2.setName("temperature");
+////        property2.setMqttTopic("cc3200/1111/temperature");
+//        properties.add(property2);
+//        model.setProperties(properties);
+//
+//        List<Event> events = new ArrayList<>();
+//        Event event1 = new Event();
+//        event1.setName("light-on");
+////        event1.setMqttTopic("cc3200/1111/light-on");
+//        events.add(event1);
+//
+//        Event event2 = new Event();
+//        event2.setName("light-off");
+////        event2.setMqttTopic("cc3200/1111/light-off");
+//        events.add(event2);
+//
+//        model.setEvents(events);
+//
+//        DeviceModel deviceModel = new DeviceModel();
+//        deviceModel.setModel(model);
+////        KafkaConfig kafkaConfig = new KafkaConfig();
+////        kafkaConfig.setServer("192.168.123.131:9092");
+////        kafkaConfig.setGroupId("1");
+//////        List<String> kafkaTopics = new ArrayList<>();
+////        kafkaConfig.setTopic("cc3200/1111");
+//////        kafkaConfig.setTopics(kafkaTopics);
+////        deviceModel.setKafkaConfig(kafkaConfig);
+//
+////        MqttConfig mqttConfig = new MqttConfig();
+////        mqttConfig.setBrokerUrl("tcp://192.168.123.247:1883");
+////        mqttConfig.setClientId("12345");
+//////        List<String> mqttTopics = new ArrayList<>();
+////        mqttConfig.setTopic("cc3200/1111/#");
+//////        mqttTopics.add("cc3200/1111/temperature");
+//////        mqttConfig.setTopics( mqttTopics);
+////        deviceModel.setMqttConfig(mqttConfig);
+//
+//
+//        String str = JSON.toJSONString(deviceModel);
+//
+//        map.put("name","cc3200-1111");
+//        map.put("model", str);
+//        doc.setDocMap(map);
+//        doc.setConnName("model");
+//        doc.setCollectionName("model");
+////        System.out.println("11111");
+//        mongoDBActorRef.tell(doc);
+//        return CompletableFuture.completedFuture(Done.getInstance());
+//    }
+//
+//    private CompletionStage<Done> test() {
+//        InsertMongoDBDoc doc = new InsertMongoDBDoc();
+//        Map<String,String> map = new HashMap<>();
+//        map.put("name", "ling");
+//        doc.setDocMap(map);
+//        doc.setConnName("test");
+//        doc.setCollectionName("test");
 //        System.out.println("11111");
-        mongoDBActorRef.tell(doc);
-        return CompletableFuture.completedFuture(Done.getInstance());
-    }
-
-    private CompletionStage<Done> test() {
-        InsertMongoDBDoc doc = new InsertMongoDBDoc();
-        Map<String,String> map = new HashMap<>();
-        map.put("name", "ling");
-        doc.setDocMap(map);
-        doc.setConnName("test");
-        doc.setCollectionName("test");
-        System.out.println("11111");
-        mongoDBActorRef.tell(doc);
-        return CompletableFuture.completedFuture(Done.getInstance());
-    }
+//        mongoDBActorRef.tell(doc);
+//        return CompletableFuture.completedFuture(Done.getInstance());
+//    }
 }
